d8d3770beffc09fb1a1499d4044aa25964b475ed
Vitaly Kovalyshyn <kovalyshyn@users.noreply.github.com>
2020-09-28 14:48:44 +0300

    Added HOST_NAME env

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 curl -so /app/swagger.json https://raw.githubusercontent.com/navrotskyj/webitel_sdk/master/swagger/api.json
 
+if [ "$HOST_NAME" ]; then
+    sed -i 's/dev.webitel.com/'$HOST_NAME'/g' /app/swagger.json
+fi
+
 /usr/share/nginx/run.sh
